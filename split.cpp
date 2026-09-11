30bc3d643bc8a8fe3149d7c836910e6855553174
Andrew Chan <achan391@usc.edu>
2026-09-11 05:39:54 +0000

    doneski

--- split.cpp
+++ split.cpp
@@ -11,13 +11,30 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
-
+#include <cstddef>
 /* Add a prototype for a helper function here if you need */
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+
+  if(in == NULL){
+    return;
+  }
+
+  Node* current = in;
+  in = in->next;
+
+  split(in, odds, evens);
+  if(current->value % 2 == 0){
+    current->next = evens;
+    evens = current; 
+  }
+  else{
+    current->next = odds;
+    odds = current; 
+  }
 }
 
 /* If you needed a helper function, write it here */
